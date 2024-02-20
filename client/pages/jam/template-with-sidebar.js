070ca60a743cde2715516bd5d1dc60bdba14dcfb
antonio88118 <yangan88118@gmail.com>
2024-02-20 20:50:05 +0800

    新增頂部功能列 top-function

--- client/pages/jam/template-with-sidebar.js
+++ client/pages/jam/template-with-sidebar.js
@@ -4,6 +4,11 @@ import Footer from '@/components/common/footer'
 import Link from 'next/link'
 import Image from 'next/image'
 import jamHero from '@/assets/jam-hero.png'
+import { IoHome } from 'react-icons/io5'
+import { FaChevronRight } from 'react-icons/fa6'
+import { IoIosSearch } from 'react-icons/io'
+import { FaFilter } from 'react-icons/fa6'
+import { FaSortAmountDown } from 'react-icons/fa'
 
 // sidebar假資料
 const sidebarData = ['吉他', '貝斯', '鍵盤樂器', '打擊樂器']
@@ -18,7 +23,7 @@ export default function Test() {
       <div className="container">
         <div className="row">
           {/* sidebar */}
-          <div className="sidebar-wrapper col-sm-2">
+          <div className="sidebar-wrapper d-none d-sm-block  col-sm-2">
             <div className="sidebar">
               <ul className="d-flex flex-column">
                 {sidebarData.map((item, index) => {
@@ -34,23 +39,62 @@ export default function Test() {
 
           {/* 頁面內容 */}
           <main
-            className="main col-sm-10"
+            className="col-12 col-sm-10 pe-0"
             style={{
-              backgroundColor: 'rgb(195, 195, 195)',
               paddingLeft: '30px',
             }}
           >
-            主要內容
+            {/* 頂部功能列 */}
+            <div className="top-function-container">
+              {/* 麵包屑 */}
+              <div className="breadcrumb-wrapper">
+                <ul className="d-flex align-items-center p-0 m-0">
+                  <IoHome size={20} />
+                  <li style={{ marginLeft: '8px' }}>Let&apos;s JAM!</li>
+                  <FaChevronRight />
+                  <li style={{ marginLeft: '10px' }}>團員募集</li>
+                </ul>
+              </div>
+
+              <div className="d-flex justify-content-between">
+                {/* 搜尋欄 */}
+                <div className="search input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="請輸入關鍵字..."
+                  />
+                  <div className="search-btn btn d-flex justify-content-center align-items-center p-0">
+                    <IoIosSearch size={25} />
+                  </div>
+                </div>
+
+                <div className="filter-sort d-flex justify-content-between">
+                  {/* 條件篩選 */}
+                  <div className="filter d-flex align-items-center">
+                    條件篩選
+                    <FaFilter size={13} />
+                  </div>
+                  {/* 資料排序 */}
+                  <div className="sort d-flex justify-content-between align-items-center">
+                    <div className="d-flex align-items-center">
+                      排序
+                      <FaSortAmountDown size={13} />
+                    </div>
+                    <div className="sort-item active">新到舊</div>
+                    <div className="sort-item">舊到新</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* 主內容 */}
+            <div className="content"></div>
           </main>
         </div>
       </div>
       <Footer />
 
-      <style jsx>{`
-        .main {
-          min-height: 100svh;
-        }
-      `}</style>
+      <style jsx>{``}</style>
     </>
   )
 }
