5b897d2c938057b6cdfda6a68077de8be01213f6
antonio88118 <yangan88118@gmail.com>
2024-02-23 11:05:51 +0800

    更新條件篩選 filter: 表單可控、清除內容、點擊空白處關閉

--- client/pages/jam/template-with-sidebar.js
+++ client/pages/jam/template-with-sidebar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '@/components/common/navbar'
 import Footer from '@/components/common/footer'
 import Link from 'next/link'
@@ -13,6 +13,20 @@ import { CgClose } from 'react-icons/cg'
 
 export default function Test() {
   const [filterVisible, setFilterVisible] = useState(false)
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      setFilterVisible(false)
+    })
+  }, [])
+  // 阻止事件冒泡造成篩選表單關閉
+  const stopPropagation = (e) => {
+    e.stopPropagation()
+  }
+  // 顯示表單
+  const onshow = (e) => {
+    stopPropagation(e)
+    setFilterVisible(!filterVisible)
+  }
   // ----------------------假資料  ----------------------
   // sidebar假資料
   const sidebarData = ['吉他', '貝斯', '鍵盤樂器', '打擊樂器']
@@ -24,17 +38,26 @@ export default function Test() {
     { id: 3, name: 'Fender' },
     { id: 4, name: 'Gibson' },
   ]
-  const [brandSelect, setBrandSelect] = useState('')
+  const [brandSelect, setBrandSelect] = useState('all')
 
   const [priceLow, setPriceLow] = useState('')
   const [priceHigh, setPriceHigh] = useState('')
 
   // 課程評價
-  const scoreState = ['全部', '5', '4', '3']
-  const [score, setScore] = useState('全部')
+  const scoreState = ['all', '5', '4', '3']
+  const [score, setScore] = useState('all')
 
   // 活動促銷
   const [sales, setSales] = useState(false)
+
+  // 清除表單內容
+  const cleanFilter = () => {
+    setBrandSelect('all')
+    setPriceLow('')
+    setPriceHigh('')
+    setScore('all')
+    setSales(false)
+  }
 
   return (
     <>
@@ -103,128 +126,125 @@ export default function Test() {
                   >
                     <div
                       className="filter-text d-flex align-items-center mx-2"
-                      onClick={() => {
-                        setFilterVisible(true)
-                      }}
+                      role="presentation"
+                      onClick={onshow}
                     >
                       條件篩選
                       <FaFilter size={13} />
-                    </div>
-                    <div
-                      className={`filter ${
-                        filterVisible === false ? 'd-none' : 'd-block'
-                      }`}
-                    >
-                      <div className="d-flex justify-content-end">
-                        <CgClose
-                          className="filter-close"
-                          size={26}
-                          onClick={() => {
-                            setFilterVisible(false)
-                          }}
-                        />
-                      </div>
-                      {/* 品牌 */}
-                      <div className="filter-item">
-                        <div className="filter-title">選擇品牌</div>
-                        <select
-                          className="form-select"
-                          aria-label="Default select example"
-                          value={brandSelect}
-                          name="brand"
-                          onChange={(e) => {
-                            setBrandSelect(e.target.value)
-                          }}
-                        >
-                          <option selected value="all">
-                            全部
-                          </option>
-                          {brandData.map((v) => {
-                            return (
-                              <option key={v.id} value={v.id}>
-                                {v.name}
-                              </option>
-                            )
-                          })}
-                        </select>
-                      </div>
-                      {/* 價格區間 */}
-                      <div className="filter-item">
-                        <div className="filter-title">價格區間</div>
-                        <input
-                          type="number"
-                          className="form-control mb-2"
-                          placeholder="最低價"
-                          name="priceLow"
-                          value={priceLow}
-                          min={0}
-                          max={priceHigh - 1}
-                          onChange={(e) => {
-                            setPriceLow(e.target.value)
-                          }}
-                        />
-                        <input
-                          type="number"
-                          className="form-control"
-                          placeholder="最高價"
-                          name="priceHigh"
-                          value={priceHigh}
-                          min={priceLow + 1}
-                          onChange={(e) => {
-                            setPriceHigh(e.target.value)
-                          }}
-                        />
-                      </div>
-                      {/* 商品評價 */}
-                      <div className="filter-item m-0">
-                        <div className="filter-title">商品評價</div>
-                        <div className="filter-radio-group d-flex flex-wrap justify-content-between">
-                          {scoreState.map((v, i) => {
-                            return (
-                              <div
-                                className="filter-radio-item form-check p-0 mb-3"
-                                key={i}
-                              >
-                                <label className="form-check-label">
-                                  <input
-                                    classname="form-check-input"
-                                    type="radio"
-                                    name="score"
-                                    value={v}
-                                    onChange={(e) => {
-                                      setScore(e.target.value)
-                                    }}
-                                  />
-                                  &nbsp;{v === '全部' ? v : v + '星'}
-                                </label>
-                              </div>
-                            )
-                          })}
+                      <div
+                        className={`filter ${
+                          filterVisible === false ? 'd-none' : 'd-block'
+                        }`}
+                        onClick={stopPropagation}
+                        role="presentation"
+                      >
+                        {/* 品牌 */}
+                        <div className="filter-item">
+                          <div className="filter-title">選擇品牌</div>
+                          <select
+                            className="form-select"
+                            aria-label="Default select example"
+                            value={brandSelect}
+                            name="brand"
+                            onChange={(e) => {
+                              setBrandSelect(e.target.value)
+                            }}
+                          >
+                            <option selected value="all">
+                              全部
+                            </option>
+                            {brandData.map((v) => {
+                              return (
+                                <option key={v.id} value={v.id}>
+                                  {v.name}
+                                </option>
+                              )
+                            })}
+                          </select>
                         </div>
-                      </div>
-                      {/* 促銷商品 */}
-                      <div className="filter-item">
-                        <div className="form-check">
-                          <label className="form-check-label filter-title mb-0">
-                            <input
-                              className="form-check-input"
-                              type="checkbox"
-                              value={sales}
-                              name="sales"
-                              onChange={() => {
-                                setSales(!sales)
-                              }}
-                            />{' '}
-                            促銷商品
-                          </label>
+                        {/* 價格區間 */}
+                        <div className="filter-item">
+                          <div className="filter-title">價格區間</div>
+                          <input
+                            type="number"
+                            className="form-control mb-2"
+                            placeholder="最低價"
+                            name="priceLow"
+                            value={priceLow}
+                            min={0}
+                            max={priceHigh - 1}
+                            onChange={(e) => {
+                              setPriceLow(e.target.value)
+                            }}
+                          />
+                          <input
+                            type="number"
+                            className="form-control"
+                            placeholder="最高價"
+                            name="priceHigh"
+                            value={priceHigh}
+                            min={priceLow + 1}
+                            onChange={(e) => {
+                              setPriceHigh(e.target.value)
+                            }}
+                          />
                         </div>
-                      </div>
-                      <div className="d-flex justify-content-between gap-2">
-                        <div className="filter-btn clean-btn w-100 d-flex justify-content-center">
-                          清除
+                        {/* 商品評價 */}
+                        <div className="filter-item m-0">
+                          <div className="filter-title">商品評價</div>
+                          <div className="filter-radio-group d-flex flex-wrap justify-content-between">
+                            {scoreState.map((v, i) => {
+                              return (
+                                <div
+                                  className="filter-radio-item form-check p-0 mb-3"
+                                  key={i}
+                                >
+                                  <label className="form-check-label">
+                                    <input
+                                      classname="form-check-input"
+                                      type="radio"
+                                      name="score"
+                                      value={v}
+                                      checked={v === score}
+                                      onChange={(e) => {
+                                        setScore(e.target.value)
+                                      }}
+                                    />
+                                    &nbsp;{v === 'all' ? '全部' : v + '星'}
+                                  </label>
+                                </div>
+                              )
+                            })}
+                          </div>
                         </div>
-                        <div className="filter-btn confirm-btn w-100 d-flex justify-content-center">
-                          確認
+                        {/* 促銷商品 */}
+                        <div className="filter-item">
+                          <div className="form-check">
+                            <label className="form-check-label filter-title mb-0">
+                              <input
+                                className="form-check-input"
+                                type="checkbox"
+                                value={sales}
+                                name="sales"
+                                onChange={() => {
+                                  setSales(!sales)
+                                }}
+                              />{' '}
+                              促銷商品
+                            </label>
+                          </div>
+                        </div>
+                        <div className="d-flex justify-content-between gap-2">
+                          <div
+                            className="filter-btn clean-btn w-100 d-flex justify-content-center"
+                            role="presentation"
+                            onClick={cleanFilter}
+                          >
+                            清除
+                          </div>
+                          <div className="filter-btn confirm-btn w-100 d-flex justify-content-center">
+                            確認
+                          </div>
                         </div>
                       </div>
                     </div>
