6282ca5358be068d2d0d109f168c236491744cfb
joeisgood0819 <joeisgood0819@gmail.com>
2024-02-26 16:01:48 +0800

    joe article-card 收藏功能

--- client/components/article/article-card.js
+++ client/components/article/article-card.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
 import { FaEye } from "react-icons/fa";
+import bookmarkIconFill from '@/assets/fillbookmark.svg'
+import bookmarkIcon from '@/assets/emptybookmark.svg'
 import { FaBookmark } from "react-icons/fa";
+import data from '@/data/article.json'
+import Image from 'next/image';
+
 
 export default function Articlecard() {
+  const [discount, setDiscount] = useState('1')
+  // console.log(products)
+
+  // 擴充收藏功能
+  const initState = data.map((v, i) => {
+    return { ...v, fav: false }
+  })
+  // 擴充後的物件陣列作為初始值
+  const [articles, setArticles] = useState(initState)
+
+  // 純func
+  const handleToggleFav = (id) => {
+    const newArticles = articles.map((v, i) => {
+      if (v.id === id) return { ...v, fav: !v.fav }
+      else return v
+    })
+    setArticles(newArticles)
+  }
+
   return (
     <>
       <div className="article-card">
         {/* info區塊 */}
-        <div className="article-info d-flex justify-content-between align-items-center mb-3">
+        <div className="article-info d-flex 
+        justify-content-between align-items-center mb-3">
           <img className="article-author" src="/article/empty.png" alt="空的圖" />
-          <span className="info-p text-secondary">作者</span>
+          <span className="info-p text-secondary">Joe</span>
           <span className="info-p text-secondary">2023/11/27</span>
         </div>
         {/* article區塊 */}
@@ -42,7 +67,19 @@ export default function Articlecard() {
         <div className="kind-bookmark d-flex justify-content-between align-items-center">
           <div className="article-kind text-black bg-body px-2 pt-1 pb-1">技術</div>
           <div className="bookmark">
-            <FaBookmark />
+            {articles.map((v, i) => {
+              return (
+                <Image
+                  src={v.fav ? bookmarkIconFill : bookmarkIcon}
+                  alt=""
+                  width={25}
+                  height={25}
+                  onClick={() => {
+                    handleToggleFav(v.id)
+                  }}
+                />
+              )
+            })}
           </div>
         </div>
       </div>
@@ -83,7 +120,7 @@ export default function Articlecard() {
         font-size: small;
       }
       .bookmark{
-        font-size: large;
+        width: 50px;
       }
       `}</style>
     </>
