ec658fdf06c6d2da6498c9d1eb37df90903a1a39
SherriW27 <140508098+SherriW27@users.noreply.github.com>
2024-03-05 15:40:42 +0800

    Sherri 0305最新的server-test

--- server-test/routes/lesson.js
+++ server-test/routes/lesson.js
@@ -37,18 +37,18 @@ router.get("/categories", async (req, res) => {
   }
 });
 
-// 商品列表路由，根據分類返回相應商品列表
-router.get("/?category=${categoryId}", (req, res) => {
-  const { category } = req.query;
-  const query = "SELECT * FROM `product` WHERE `lesson_category_id` = ?"; // 假設您的商品表為 products，並且有一個字段為 category
-  db.query(query, [category], (err, results) => {
-    if (err) {
-      console.error("Error querying database:", err);
-      res.status(500).json({ error: "Internal server error" });
-      return;
-    }
-    res.json(results);
-  });
-});
+// // 商品列表路由，根據分類返回相應商品列表
+// router.get("/?category=${categoryId}", (req, res) => {
+//   const { category } = req.query;
+//   const query = "SELECT * FROM `product` WHERE `lesson_category_id` = ?"; // 假設您的商品表為 products，並且有一個字段為 category
+//   db.query(query, [category], (err, results) => {
+//     if (err) {
+//       console.error("Error querying database:", err);
+//       res.status(500).json({ error: "Internal server error" });
+//       return;
+//     }
+//     res.json(results);
+//   });
+// });
 
 export default router;
