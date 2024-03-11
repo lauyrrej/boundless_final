76960ee4829251b42843926b69dcab7850086ed1
Lee <yy85521@gmail.com>
2024-03-11 15:43:04 +0800

    0311MergePM

--- server-test/routes/coupon.js
+++ server-test/routes/coupon.js
@@ -8,44 +8,9 @@ const router = express.Router();
 // 創建一個 multer 中間件，用於處理文件上傳
 const upload = multer();
 
-// #region 宇哲的
+// #region ...
 // //取得coupon資料-升降冪：ID、價格/百分比、日期排序
-// router.get("/", async (req, res) => {
-//   try {
-//     let [couponData] = await db.execute(
-//       "SELECT * FROM `coupon` WHERE `valid` = 1 ORDER BY id ASC"
-//       // "SELECT * FROM `coupon` ORDER BY discount DESC",
-//       // "SELECT * FROM `coupon` ORDER BY limit_time ASC"
-//     );
-//     if (couponData) {
-//       res.json(couponData);
-//     } else {
-//       res.json("沒有找到相應的資訊");
-//     }
-//   } catch (error) {
-//     console.error("發生錯誤：", error);
-//     res.json("res.json發生錯誤");
-//   }
-// });
-
 // //取得coupon資料-分頁：全部、樂器、課程、已使用
-// router.get("/categories", async (req, res) => {
-//   try {
-//     let [couponData] = await db.execute(
-//       // "SELECT * FROM `coupon` WHERE `type` = 1 AND valid=1  "
-//       // "SELECT * FROM `coupon` WHERE `type` = 2 AND valid=1"
-//       "SELECT * FROM `coupon` WHERE `valid` = 0 ORDER BY id ASC"
-//     );
-//     if (couponData) {
-//       res.json(couponData);
-//     } else {
-//       res.json("沒有找到相應的資訊");
-//     }
-//   } catch (error) {
-//     console.error("發生錯誤：", error);
-//     res.json("res.json發生錯誤");
-//   }
-// });
 // #endregion
 
 // 處理 GET 請求，路徑為 /public/coupon/FindAll
