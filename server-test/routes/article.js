86f7ef8f17df757f7cc2ae29f19bdf3acaa05078
joeisgood0819 <joeisgood0819@gmail.com>
2024-03-15 14:50:47 +0800

    joe eslint server-test

--- server-test/routes/article.js
+++ server-test/routes/article.js
@@ -1,71 +1,78 @@
-import express, { json } from "express";
-import db from "../db.js";
-import cors from "cors";
+import express, { json } from 'express';
+import db from '../db.js';
+import cors from 'cors';
 // import formidable from "formidable";
-import { dirname, resolve, extname } from "path";
-import { fileURLToPath } from "url";
-import { renameSync } from "fs";
+import { dirname, resolve, extname } from 'path';
+import { fileURLToPath } from 'url';
+import { renameSync } from 'fs';
 const __dirname = dirname(fileURLToPath(import.meta.url));
-import multer from "multer";
-const upload = multer({ dest: resolve(__dirname, "public/Images") });
+import multer from 'multer';
+const upload = multer({ dest: resolve(__dirname, 'public/Images') });
 // 上傳內容放public
 
 const router = express.Router();
 router.use(cors());
 
 // 文章列表
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    let [articleData] = await db.execute("SELECT article.*, article_category.name AS category_name,article_comment.likes AS comment_likes, user.name AS user_name, user.img AS user_img, article_user.name AS article_author_name, article_user.img AS article_author_img FROM article JOIN article_category ON article.category_id = article_category.id LEFT JOIN article_comment ON article.id = article_comment.article_id LEFT JOIN user ON article_comment.user_id = user.id LEFT JOIN user AS article_user ON article.user_id = article_user.id ORDER BY article.id");
+    let [articleData] = await db.execute(
+      'SELECT article.*, article_category.name AS category_name,article_comment.likes AS comment_likes, user.name AS user_name, user.img AS user_img, article_user.name AS article_author_name, article_user.img AS article_author_img FROM article JOIN article_category ON article.category_id = article_category.id LEFT JOIN article_comment ON article.id = article_comment.article_id LEFT JOIN user ON article_comment.user_id = user.id LEFT JOIN user AS article_user ON article.user_id = article_user.id ORDER BY article.id'
+    );
     if (articleData) {
       res.json(articleData);
     } else {
-      res.json("沒有找到相應的資訊");
+      res.json('沒有找到相應的資訊');
     }
   } catch (error) {
-    console.error("發生錯誤：", error);
-    res.json("發生錯誤" + error);
+    console.error('發生錯誤：', error);
+    res.json('發生錯誤' + error);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   let auid = req.params.id;
   let [data] = await db
-    .execute("SELECT article.*, article_category.name AS category_name, article_comment.content AS comment_content,article_comment.created_time AS comment_created_time,article_comment.likes AS comment_likes, user.name AS user_name, user.img AS user_img FROM article JOIN article_category ON article.category_id = article_category.id LEFT JOIN article_comment ON article.id = article_comment.article_id LEFT JOIN user ON article_comment.user_id = user.id WHERE article.auid = ?", [auid])
+    .execute(
+      'SELECT article.*, article_category.name AS category_name, article_comment.content AS comment_content,article_comment.created_time AS comment_created_time,article_comment.likes AS comment_likes, user.name AS user_name, user.img AS user_img FROM article JOIN article_category ON article.category_id = article_category.id LEFT JOIN article_comment ON article.id = article_comment.article_id LEFT JOIN user ON article_comment.user_id = user.id WHERE article.auid = ?',
+      [auid]
+    )
     .catch(() => {
       return undefined;
     });
   if (data) {
     res.status(200).json(data);
-    console.log(data)
+    console.log(data);
   } else {
-    res.status(400).send("發生錯誤");
+    res.status(400).send('發生錯誤');
   }
 });
 
 // article_category
-router.get("/categories", async (req, res) => {
+router.get('/categories', async (req, res) => {
   try {
-    let [articleCategory] = await db.execute("SELECT * FROM `article_category`");
+    let [articleCategory] = await db.execute(
+      'SELECT * FROM `article_category`'
+    );
     if (articleCategory) {
       res.json(articleCategory);
     } else {
-      res.json("沒有找到相應的資訊");
+      res.json('沒有找到相應的資訊');
     }
   } catch (error) {
-    console.error("發生錯誤：", error);
-    res.json("發生錯誤" + error);
+    console.error('發生錯誤：', error);
+    res.json('發生錯誤' + error);
   }
 });
 
-// router.post("/api/upload", upload.single("myFile"), (req, res) => {
-//   // 處理上傳邏輯與命名myFile=前端input:name
-//   let timestamp = Date.now();
-//   let newFileName = timestamp + extname(req.file.originalname);
-//   // 根據時間點給予新的名稱
-//   renameSync(req.file.path, resolve(__dirname, "public/upload", newFileName));
-//   res.json({ body: req.body, file: req.file });
-// });
+router.post('/api/upload', upload.single('myFile'), (req, res) => {
+  // 處理上傳邏輯與命名myFile=前端input:name
+  let timestamp = Date.now();
+  let newFileName = timestamp + extname(req.file.originalname);
+  // 根據時間點給予新的名稱
+  renameSync(req.file.path, resolve(__dirname, 'public/upload', newFileName));
+  res.json({ body: req.body, file: req.file });
+});
 
 // publish
 router.post(
