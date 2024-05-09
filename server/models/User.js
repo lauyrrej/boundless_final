076dcf28562e936a4a25d00a9c263a4ecd034ea1
lauyrrej <crafg1122@gmail.com>
2024-05-09 16:03:09 +0800

    更新

--- server/models/User.js
+++ server/models/User.js
@@ -15,10 +15,7 @@ export default async function (sequelize) {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+      
       password: {
         type: DataTypes.STRING,
         allowNull: true,
@@ -27,68 +24,40 @@ export default async function (sequelize) {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      avatar: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      birth_date: {
-        type: DataTypes.DATEONLY, //只需要日期
-        allowNull: true,
-      },
-      sex: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      phone: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      postcode: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+      
       google_uid: {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      line_uid: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+      
       photo_url: {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      line_access_token: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
+      
     },
     {
-      hooks: {
-        // 建立時產生密碼加密字串用
-        beforeCreate: async (user) => {
-          if (user.password) {
-            user.password = await generateHash(user.password)
-          }
-        },
-        // 更新時產生密碼加密字串用
-        beforeUpdate: async (user) => {
-          if (user.password) {
-            user.password = await generateHash(user.password)
-          }
-        },
-      },
+      // 暫改不產生加密密碼
+      // hooks: {
+      //   // 建立時產生密碼加密字串用
+      //   beforeCreate: async (user) => {
+      //     if (user.password) {
+      //       user.password = await generateHash(user.password)
+      //     }
+      //   },
+      //   // 更新時產生密碼加密字串用
+      //   beforeUpdate: async (user) => {
+      //     if (user.password) {
+      //       user.password = await generateHash(user.password)
+      //     }
+      //   },
+      // },
       tableName: 'user', //直接提供資料表名稱
       timestamps: true, // 使用時間戳
       paranoid: false, // 軟性刪除
       underscored: true, // 所有自動建立欄位，使用snake_case命名
-      createdAt: 'created_at', // 建立的時間戳
-      updatedAt: 'updated_at', // 更新的時間戳
+      createdAt: 'created_time', // 建立的時間戳
+      updatedAt: 'updated_time', // 更新的時間戳
     }
   )
 }
